b391f5d5f0a022b44627b0b840385b4e96a471f7
SamirDiegoChavezCaceres <schavezca@unsa.edu.pe>
2021-12-05 13:05:55 -0500

    Update de Vertice

--- Codigo/Vertice.java
+++ Codigo/Vertice.java
@@ -1,84 +1,36 @@
+package ADA_PyFinal.Codigo;
 
-import java.util.LinkedList;
+public class Vertice<E> implements Comparable<Vertice<E>> {
 
-public class Vertice<T> {
-	
-	// Atributos de la clase Vertice
-	
-	protected T data;
-	protected LinkedList<Arista<T>> listaAdyacencia;
-	protected int label; // 0 => Sin explorar, 1 => Explorada
-	
-	// Constructor de la clase Vertice
-	
-	public Vertice(T data) {
-		
-		setData(data);
-		setLista();
-		
-	}
-	
-	// Setters y getters de la clase Vertice
-	
-	public T getData() {
-		
-		return this.data;
-		
-	}
-	
-	public void setData(T data) {
-		
-		this.data = data;
-		
-	}
-	
-	public LinkedList<Arista<T>> getLista(){
-		
-		return this.listaAdyacencia;
-		
-	}
-	
-	public void setLista() {
-		
-		this.listaAdyacencia = new LinkedList<Arista<T>>();
-		
-	}
-	
-	public int getLabel() {
-		
-		return this.label;
-		
-	}
-	
-	public void setLabel(int label) {
-		
-		this.label = label;
-		
-	}
-	
-	// Metodos de la clase Vertice
-	
-	@SuppressWarnings("unchecked")
-	
-	public boolean equals(Object obj) {
-		
-		if(obj instanceof Vertice<?>) {
-			
-			Vertice<T> vertice = (Vertice<T>) obj;
-			return getData().equals(vertice.getData());
-			
-		}
-		
-		return false;
-	}
-	
-	// Metodo toString, devuelve el contenido del Vertice
-	
-	@Override
-	public String toString() {
-		
-		return getData() + " ==> " + getLista() + "\n";
-		
-	}
-	
+    protected E data;
+    protected ListEnlazada<Arista<E>> listArt;
+    protected int label; //0 unexp , 1 explored, 2 otro
+    protected int dist; //para algoritmos
+    protected Vertice<E> path;
+
+    public Vertice(E data) {
+        this.data = data;
+        listArt = new ListEnlazada<Arista<E>>();
+    }
+
+    public E getData() {
+        return data;
+    }
+
+    public boolean equals(Object o) {
+        if (o instanceof Vertice<?>) {
+            Vertice<E> v = (Vertice<E>) o;
+            return this.data.equals(v.data);
+        }
+        return false;
+    }
+
+    public String toString() {
+        return this.data + " <" + label + "> --> " + this.listArt.toString() + "\n";
+    }
+
+    public int compareTo(Vertice<E> v) {
+        return this.dist - v.dist;
+    }
+
 }
