7bc39c6f38583cd8200d7760fa44bd7a3d7a26c3
dneirac <dneirac@unsa.edu.pe>
2021-12-15 12:28:46 -0500

    creacion del frame e impotacion de las librerias necesarias

--- Codigo/Grafico.java
+++ Codigo/Grafico.java
@@ -1,6 +1,20 @@
 
-public class Grafico{
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class Grafico extends JFrame implements ActionListener{
+  public Grafico() {
+    setSize(1060, 3600); // size del frame
+    setTitle("Interface Proyecto final ADA"); // estableciendo el titulo
+    setDefaultCloseOperation(EXIT_ON_CLOSE); // la opcion de cerrado mediante un 'X' en la parte superior derecha
+    setVisible(true); // para que pueda ser visible
+
+  }
+  public void actionPerformed(ActionEvent e){
+
+  }
   public static void main (String[]args){
+    new Grafico();
 
   }
 }
