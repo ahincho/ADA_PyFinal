95bde9495da64ea88e8537ead4d284cf1b5f38c6
dneirac <dneirac@unsa.edu.pe>
2021-12-16 16:11:00 -0500

    creacion de los metodos distance y fillIn

--- Codigo/Grafo.java
+++ Codigo/Grafo.java
@@ -42,7 +42,7 @@ public class Grafo<E> {
         }
     }
 
-    public void Dijkstra(E info) {
+    public void Dijkstra(E info, E destiny) {
         PriorityQueue<Vertice<E>> q = new PriorityQueue<Vertice<E>>();
 
         Vertice<E> u = this.listVert.contiene(new Vertice<E>(info)).valor;
@@ -74,6 +74,8 @@ public class Grafo<E> {
             }
         }
         printDijkstra();
+        System.out.println("salida unica");
+        distance(destiny);
     }
 
     public void printDijkstra() {
@@ -89,7 +91,57 @@ public class Grafo<E> {
         }
     }
 
+    public void distance(E destiny){
+        Enlace<Vertice<E>> aux = this.listVert.cabeza();
+        //System.out.println("\nVertex\t\tDistance(km)\tPath");
+        for (; aux != null; aux = aux.siguiente) {
+            Vertice<E> ver = aux.valor;
+            if (ver.data == destiny) {
+                System.out.println(ver.data + "\t\t" + ver.dist);
+                break;
+            }
+        }
+    }
+
     public String toString() {
         return this.listVert.toString();
+    }
+
+    // Agregando metodo fillIn
+    public static Grafo<String> fillIn(){
+        Grafo<String> g = new Grafo<String>();
+
+        //TEST DIJKSTRA 
+        g.insertVert("Arequipa");
+        g.insertVert("Camana  ");
+        g.insertVert("Caraveli");
+        g.insertVert("Castilla");
+        g.insertVert("Caylloma");
+        g.insertVert("Condesuyos");
+        g.insertVert("Islay   ");
+        g.insertVert("La Union");
+        String[] ciudades 
+                = {"Arequipa","Camana  "
+                  ,"Caraveli","Castilla"
+                  ,"Caylloma","Condesuyos"
+                  ,"Islay   ","La Union",};
+        
+        int[][] caminos 
+            = {{999,175,383,177,167,227,124,369},
+            {177,999,209,123,296,173,112,315},
+            {385,210,999,331,503,380,320,360},
+            {179,123,331,999,297,50,213,192},
+            {163,295,503,297,999,347,277,400},
+            {228,173,380,50,347,999,262,142},
+            {125,112,320,213,275,262,999,405},
+            {371,315,523,192,400,142,405,999},};
+        
+        for(int i = 0; i < caminos.length ; i++)
+            for(int j = 0; j < caminos[0].length; j++)
+                g.insertArista(ciudades[i]
+                        , ciudades[j]
+                        , caminos[i][j]
+                        ,true);
+        return g;
     }
 }
