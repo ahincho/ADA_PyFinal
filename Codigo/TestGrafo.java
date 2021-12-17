5fd0e3460f6d7e29027ded0db7e7014d233dca03
dneirac <dneirac@unsa.edu.pe>
2021-12-17 09:22:18 -0500

    agregando más casos de pruba

--- Codigo/TestGrafo.java
+++ Codigo/TestGrafo.java
@@ -5,5 +5,9 @@ public class TestGrafo {
         Grafo<String> g = new Grafo<String>();
         g = g.fillIn();
         System.out.println(g.Dijkstra("La Union", "Arequipa"));
+        System.out.println(g.Dijkstra("La Union", "Castilla"));
+        System.out.println(g.Dijkstra("La Union", "Camana"));
+        System.out.println(g.Dijkstra("La Union", "Islay"));
+        System.out.println(g.Dijkstra("La Union", "Condesuyos"));
     }
 }
